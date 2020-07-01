5ff79923de02cd7f65884921d2a413c2e7e16551
dave14305 <44532942+dave14305@users.noreply.github.com>
2020-06-30 20:48:38 -0400

    Version 0.8.4
    Check webui ASP page for differences during update check (@maghuro)
    Differentiate between version change and hotfix during update (@maghuro)
    Disable auto-refresh when total tracked connections exceeds 750. Allow user to re-enable auto-refresh
    Sort wildcard appdb rules at the bottom of the rules list when saving so rules apply properly to connection table
    Reorganize custom rule evaluation to ensure application in proper order
    Added version number to WebUI heading
    
    NOTE: If more than one iptables rule matches a single connection, the last matching rule will determine the actual QoS class. Previous beta versions of FlexQoS always chose the first matching rule, which isn't in harmony with how iptables works.

--- flexqos.sh
+++ flexqos.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # FlexQoS maintained by dave14305
-version=0.8.3
+version=0.8.4
 release=06/30/2020
 # Forked from FreshJR_QOS v8.8, written by FreshJR07 https://github.com/FreshJR07/FreshJR_QOS
 #
