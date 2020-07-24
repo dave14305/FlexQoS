f4039522638cc064288df58513b8a9060ed5152c
dave14305 <44532942+dave14305@users.noreply.github.com>
2020-07-24 18:59:45 -0400

    Version 0.9.5 NEW: Added warning if download/upload minimum reserved bandwidths exceed 100% NEW: Added colors to CLI menu system (I'm not artistic; suggestions welcome) CHANGED: Added placeholder text to AppDB search box to clarify purpose CHANGED: Improved backup/restore prompts

--- flexqos.sh
+++ flexqos.sh
@@ -11,8 +11,8 @@
 ##################################################################
 # FlexQoS maintained by dave14305
 # Contributors: @maghuro
-version=0.9.4
-release=2020-07-14
+version=0.9.5
+release=2020-07-24
 # Forked from FreshJR_QOS v8.8, written by FreshJR07 https://github.com/FreshJR07/FreshJR_QOS
 #
 # Script Changes Unidentified traffic destination away from "Defaults" into "Others"
