992501059184473eb0ac39ff0cb744100386e35a
dave14305 <44532942+dave14305@users.noreply.github.com>
2020-08-14 10:01:00 -0400

    Simplify repetitive printf commands

--- flexqos.sh
+++ flexqos.sh
@@ -135,55 +135,27 @@ appdb_static_rules() {
 
 write_custom_rates() {
 	{
-		printf "${tc} class change dev br0 parent 1:1 classid 1:10 htb $PARMS prio 0 rate ${DownRate0}Kbit ceil ${DownCeil0}Kbit burst $DownBurst0 cburst $DownCburst0"
-		[ "$DownQuantum0" != "default" ] && printf " quantum $DownQuantum0"
-		printf "\n"
-		printf "${tc} class change dev br0 parent 1:1 classid 1:11 htb $PARMS prio 1 rate ${DownRate1}Kbit ceil ${DownCeil1}Kbit burst $DownBurst1 cburst $DownCburst1"
-		[ "$DownQuantum1" != "default" ] && printf " quantum $DownQuantum1"
-		printf "\n"
-		printf "${tc} class change dev br0 parent 1:1 classid 1:12 htb $PARMS prio 2 rate ${DownRate2}Kbit ceil ${DownCeil2}Kbit burst $DownBurst2 cburst $DownCburst2"
-		[ "$DownQuantum2" != "default" ] && printf " quantum $DownQuantum2"
-		printf "\n"
-		printf "${tc} class change dev br0 parent 1:1 classid 1:13 htb $PARMS prio 3 rate ${DownRate3}Kbit ceil ${DownCeil3}Kbit burst $DownBurst3 cburst $DownCburst3"
-		[ "$DownQuantum3" != "default" ] && printf " quantum $DownQuantum3"
-		printf "\n"
-		printf "${tc} class change dev br0 parent 1:1 classid 1:14 htb $PARMS prio 4 rate ${DownRate4}Kbit ceil ${DownCeil4}Kbit burst $DownBurst4 cburst $DownCburst4"
-		[ "$DownQuantum4" != "default" ] && printf " quantum $DownQuantum4"
-		printf "\n"
-		printf "${tc} class change dev br0 parent 1:1 classid 1:15 htb $PARMS prio 5 rate ${DownRate5}Kbit ceil ${DownCeil5}Kbit burst $DownBurst5 cburst $DownCburst5"
-		[ "$DownQuantum5" != "default" ] && printf " quantum $DownQuantum5"
-		printf "\n"
-		printf "${tc} class change dev br0 parent 1:1 classid 1:16 htb $PARMS prio 6 rate ${DownRate6}Kbit ceil ${DownCeil6}Kbit burst $DownBurst6 cburst $DownCburst6"
-		[ "$DownQuantum6" != "default" ] && printf " quantum $DownQuantum6"
-		printf "\n"
-		printf "${tc} class change dev br0 parent 1:1 classid 1:17 htb $PARMS prio 7 rate ${DownRate7}Kbit ceil ${DownCeil7}Kbit burst $DownBurst7 cburst $DownCburst7"
-		[ "$DownQuantum7" != "default" ] && printf " quantum $DownQuantum7"
-		printf "\n"
-
-		printf "${tc} class change dev $tcwan parent 1:1 classid 1:10 htb $PARMS prio 0 rate ${UpRate0}Kbit ceil ${UpCeil0}Kbit burst $UpBurst0 cburst $UpCburst0"
-		[ "$UpQuantum0" != "default" ] && printf " quantum $UpQuantum0"
-		printf "\n"
-		printf "${tc} class change dev $tcwan parent 1:1 classid 1:11 htb $PARMS prio 1 rate ${UpRate1}Kbit ceil ${UpCeil1}Kbit burst $UpBurst1 cburst $UpCburst1"
-		[ "$UpQuantum1" != "default" ] && printf " quantum $UpQuantum1"
-		printf "\n"
-		printf "${tc} class change dev $tcwan parent 1:1 classid 1:12 htb $PARMS prio 2 rate ${UpRate2}Kbit ceil ${UpCeil2}Kbit burst $UpBurst2 cburst $UpCburst2"
-		[ "$UpQuantum2" != "default" ] && printf " quantum $UpQuantum2"
-		printf "\n"
-		printf "${tc} class change dev $tcwan parent 1:1 classid 1:13 htb $PARMS prio 3 rate ${UpRate3}Kbit ceil ${UpCeil3}Kbit burst $UpBurst3 cburst $UpCburst3"
-		[ "$UpQuantum3" != "default" ] && printf " quantum $UpQuantum3"
-		printf "\n"
-		printf "${tc} class change dev $tcwan parent 1:1 classid 1:14 htb $PARMS prio 4 rate ${UpRate4}Kbit ceil ${UpCeil4}Kbit burst $UpBurst4 cburst $UpCburst4"
-		[ "$UpQuantum4" != "default" ] && printf " quantum $UpQuantum4"
-		printf "\n"
-		printf "${tc} class change dev $tcwan parent 1:1 classid 1:15 htb $PARMS prio 5 rate ${UpRate5}Kbit ceil ${UpCeil5}Kbit burst $UpBurst5 cburst $UpCburst5"
-		[ "$UpQuantum5" != "default" ] && printf " quantum $UpQuantum5"
-		printf "\n"
-		printf "${tc} class change dev $tcwan parent 1:1 classid 1:16 htb $PARMS prio 6 rate ${UpRate6}Kbit ceil ${UpCeil6}Kbit burst $UpBurst6 cburst $UpCburst6"
-		[ "$UpQuantum6" != "default" ] && printf " quantum $UpQuantum6"
-		printf "\n"
-		printf "${tc} class change dev $tcwan parent 1:1 classid 1:17 htb $PARMS prio 7 rate ${UpRate7}Kbit ceil ${UpCeil7}Kbit burst $UpBurst7 cburst $UpCburst7"
-		[ "$UpQuantum7" != "default" ] && printf " quantum $UpQuantum7"
-		printf "\n"
+		for i in 0 1 2 3 4 5 6 7
+		do
+			eval DownRate=\$DownRate$i
+			eval DownCeil=\$DownCeil$i
+			eval DownBurst=\$DownBurst$i
+			eval DownCburst=\$DownCburst$i
+			eval DownQuantum=\$DownQuantum$i
+			printf "${tc} class change dev %s parent 1:1 classid 1:1%s htb %s prio %s rate %sKbit ceil %sKbit burst %s cburst %s" \
+					"br0" "$i" "$PARMS" "$i" "$DownRate" "$DownCeil" "$DownBurst" "$DownCburst"
+			[ "$DownQuantum" != "default" ] && printf " quantum %s" "$DownQuantum"
+			printf "\n"
+			eval UpRate=\$UpRate$i
+			eval UpCeil=\$UpCeil$i
+			eval UpBurst=\$UpBurst$i
+			eval UpCburst=\$UpCburst$i
+			eval UpQuantum=\$UpQuantum$i
+			printf "${tc} class change dev %s parent 1:1 classid 1:1%s htb %s prio %s rate %sKbit ceil %sKbit burst %s cburst %s" \
+					"$tcwan" "$i" "$PARMS" "$i" "$UpRate" "$UpCeil" "$UpBurst" "$UpCburst"
+			[ "$UpQuantum" != "default" ] && printf " quantum %s" $UpQuantum
+			printf "\n"
+		done
 	} >> /tmp/${SCRIPTNAME}_tcrules
 } # write_custom_rates
 
