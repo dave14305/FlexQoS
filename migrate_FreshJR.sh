666f93f1677428a268b2b903e1a582ac0b4ea3e9
dave14305 <44532942+dave14305@users.noreply.github.com>
2021-01-03 20:12:40 -0500

    Update FreshJR_QOS migration script with new bandwidth parameter name and order

--- migrate_FreshJR.sh
+++ migrate_FreshJR.sh
@@ -54,10 +54,10 @@ if [ "$(nvram get fb_email_dbg | sed 's/>/;/g' | tr -cd ';' | wc -c)" = "48" ]; 
 		r4 d4 \
 		gameCIDR \
 		ruleFLAG \
-		drp0 drp1 drp2 drp3 drp4 drp5 drp6 drp7 \
-		dcp0 dcp1 dcp2 dcp3 dcp4 dcp5 dcp6 dcp7 \
-		urp0 urp1 urp2 urp3 urp4 urp5 urp6 urp7 \
-		ucp0 ucp1 ucp2 ucp3 ucp4 ucp5 ucp6 ucp7 \
+		drp0 drp2 drp5 drp1 drp4 drp7 drp3 drp6 \
+		dcp0 dcp2 dcp5 dcp1 dcp4 dcp7 dcp3 dcp6 \
+		urp0 urp2 urp5 urp1 urp4 urp7 urp3 urp6 \
+		ucp0 ucp2 ucp5 ucp1 ucp4 ucp7 ucp3 ucp6 \
 	<<EOF
 $(nvram get fb_email_dbg | sed 's/>/;/g' )
 EOF
@@ -112,7 +112,7 @@ if [ -z "$(am_settings_get flexqos_appdb)" ]; then
 fi
 
 if [ -z "$(am_settings_get flexqos_bandwidth)" ]; then
-	am_settings_set flexqos_bandwidth "<${drp0}>${drp1}>${drp2}>${drp3}>${drp4}>${drp5}>${drp6}>${drp7}<${dcp0}>${dcp1}>${dcp2}>${dcp3}>${dcp4}>${dcp5}>${dcp6}>${dcp7}<${urp0}>${urp1}>${urp2}>${urp3}>${urp4}>${urp5}>${urp6}>${urp7}<${ucp0}>${ucp1}>${ucp2}>${ucp3}>${ucp4}>${ucp5}>${ucp6}>${ucp7}"
+	am_settings_set flexqos_bwrates "<${drp0}>${drp1}>${drp2}>${drp3}>${drp4}>${drp5}>${drp6}>${drp7}<${dcp0}>${dcp1}>${dcp2}>${dcp3}>${dcp4}>${dcp5}>${dcp6}>${dcp7}<${urp0}>${urp1}>${urp2}>${urp3}>${urp4}>${urp5}>${urp6}>${urp7}<${ucp0}>${ucp1}>${ucp2}>${ucp3}>${ucp4}>${ucp5}>${ucp6}>${ucp7}"
 fi
 
 if [ ! -f "/jffs/addons/flexqos/restore_freshjr_nvram.sh" ]; then
