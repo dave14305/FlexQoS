643ecb1419af4c2ed75b59d0d028c61763756f56
dave14305 <44532942+dave14305@users.noreply.github.com>
2021-03-21 18:39:46 -0400

    Version 1.2.3
    NEW
    - Added ability to remember filter settings between sessions. See the Remember checkbox in the Filter Connections header. Saved filters are stored as browser cookies and not as a configuration setting.
    
    CHANGED
    - Made fq_codel the default qdisc option. Can be disabled by choosing "ASUS" as the QoS Mode.
    - Delay FlexQoS startup until all qdiscs are created by Adaptive QoS, since we now replace them with fq_codel by default.
    - During startup check, if QoS fails to initialize, only attempt to restart qos one time before erroring out. This avoids endless qos restart loops from happening if an unrecoverable error happens with Adaptive QoS.
    - Adopt sqm-scripts minimum quantum/burst/cburst of 1749 based on worst-case ATM overhead for one packet of MTU size.
    - Renamed File Downloads to File Transfers to more closely resemble the stock File Transferring label.
    - Renamed Game Downloads category back to Learn-From-Home. Changed built-in Game Downloads rule to use File Transferring category for new rules. Existing Game Downloads rules will still direct to the Learn-From-Home priority level.
    - Show full stock category names in CLI appdb command output.
    - Moved FlexQoS tab to the third tab between QoS and Classification.
    - All Class dropdowns are now listed in the user-defined priority order.
    - Moved version number and update check under Options panel
    - Moved graph scale option to the main page under the refresh dropdown
    
    FIXED
    - When disabling Refresh in the WebUI, don't refresh one last time.
    - Fixed Overhead dropdown display issue when using 384 firmware.

--- flexqos.sh
+++ flexqos.sh
@@ -11,7 +11,7 @@
 # FlexQoS maintained by dave14305
 # Contributors: @maghuro
 version=1.2.3
-release=2021-03-07
+release=2021-03-21
 # Forked from FreshJR_QOS v8.8, written by FreshJR07 https://github.com/FreshJR07/FreshJR_QOS
 # License
 #  FlexQoS is free to use under the GNU General Public License, version 3 (GPL-3.0).
